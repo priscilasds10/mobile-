129f74c617a7cb2d7e8abfdbda486e3c03ad12c0
priscilasds10 <priscilasds10@gmail.com>
2022-07-22 17:56:44 -0300

    first commitprojeto

---
+++ sucessor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sucessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double raio, pi=3.14;
+            double altura, volume;
+            
+            Console.WriteLine("informe a altura");
+            altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("informe o raio");
+            raio = double.Parse(Console.ReadLine());
+
+
+            volume = (raio * raio) * altura * pi;
+
+            Console.WriteLine("o volume do cilindro é:");
+            Console.WriteLine(volume);
+
+            Console.ReadKey();
+
+            }
+        }
+    }
+
